7b9ac7683e9cbaf54f8fa7550331deb2ce5936e3
Adam Baranek <adam.baranek@ci-group.org>
2019-06-11 12:00:16 -0400

    added move item functionality for extras section. was also able to remove the Show Item button and made it so that the list just updates in real time as you add items. Made a whole lot of other minor improvements.

--- shopList.js
+++ shopList.js
@@ -2,6 +2,8 @@
 
 /*Some ideas for the app:
 	- build functionality to allow moving of Extras items to a category of user's choosing (drag and drop?)
+	- build a smooth welcome page with quick instructions that the user sees before using the app
+	- remove the buttons in the printList() function when printing
 */
 
 var shoppingList = [];
@@ -12,13 +14,16 @@ var condimentsList = [];
 var coldList = [];
 var variousList = [];
 var frequentItems = {
-	'produce': ['Green Pepper', 'Sweet Potato', 'Yellow Onion', 'Mushrooms', 'Avocado'],
-	'condiments': ['Barbeque Sauce', 'Ketchup', 'Ranch', 'Mayo'],
-	'snacks': ['Hummus', 'Lara Bars', 'Corn Chips'],
+	'produce': ['Green Pepper', 'Sweet Potato', 'Yellow Onion', 'Mushrooms', 'Avocado', 'Garlic', 
+	'Broccoli Crowns', 'Hummus', 'Jalapeno', 'Tomato', 'Lime'],
+	'condiments': ['Barbeque Sauce', 'Ketchup', 'Ranch', 'Mayo', 'Olive Oil', 'Spray Oil', 
+	'Buffalo Sauce', 'Tabasco', 'Hot Sauce'],
+	'snacks': ['Lara Bars', 'Corn Chips', 'Pop Tarts', 'Candy', 'Salsa'],
 	'cold': ['Frozen Pizza', 'Bag of Fries', 'Ice Cream', 'Shredded Cheese', 'Cream Cheese', 
-			'Sour Cream', 'Cottage Cheese', 'Yoghurt'],
-	'meat': ['Chicken', 'Beef', 'Bacon', 'Eggs'],
-	'various': ['Almond Milk', 'Adam Coffee', 'Amanda Coffee', 'Tea', 'Honey', 'Cereal', 'Soup']
+			'Sour Cream', 'Cottage Cheese', 'Yoghurt', 'Peanut Butter'],
+	'meat': ['Chicken Breast', 'Beef', 'Bacon', 'Eggs', 'Chicken Thighs', 'Drumsticks'],
+	'various': ['Almond Milk', 'Adam Coffee', 'Amanda Coffee', 'Tea', 'Honey', 'Cereal', 'Soup', 
+	'Broth', 'Tortillas']
 };
 
 
@@ -32,8 +37,9 @@ function startList() {
 	while (stopAdd < 1) {
 
 		switch (addFoodItem = prompt('Add an Item and its quantity:')) {
-			case 'quit':
+			case 'done':
 				stopAdd++;
+				showList();
 				break;
 			default:
 				shoppingList.push(addFoodItem.charAt(0).toUpperCase() + addFoodItem.slice(1));
@@ -49,126 +55,247 @@ function freqItem(item) {
 		case 'Green Pepper':
 			var qty = prompt('How many?');
 			produceList.push('Green Pepper ' + qty);
+			showList();
 			break;
 		case 'Sweet Potato':
 			var qty = prompt('How many?');
 			produceList.push('Sweet Potato ' + qty);
+			showList();
 			break;
 		case 'Yellow Onion':
 			var qty = prompt('How many?');
 			produceList.push('Yellow Onion ' + qty);
+			showList();
 			break;
 		case 'Mushrooms':
 			var qty = prompt('How many? (8oz packs)');
 			produceList.push('Mushrooms ' + qty + 'pack(s)');
+			showList();
 			break;
 		case 'Avocado':
 			var qty = prompt('How many?');
 			produceList.push('Avocado ' + qty);
+			showList();
+			break;
+		case 'Garlic':
+			var qty = prompt('How many?');
+			produceList.push('Garlic ' + qty);
+			showList();
+			break;
+		case 'Broccoli Crowns':
+			var qty = prompt('How many?');
+			produceList.push('Broccoli Crowns ' + qty);
+			showList();
+			break;
+		case 'Hummus':
+			var qty = prompt('How many?');
+			produceList.push('Hummus ' + qty);
+			showList();
+			break;
+		case 'Jalapeno':
+			var qty = prompt('How many?');
+			produceList.push('Jalapeno ' + qty);
+			showList();
+			break;
+		case 'Tomato':
+			var qty = prompt('How many?');
+			produceList.push('Tomato ' + qty);
+			showList();
+			break;
+		case 'Lime':
+			var qty = prompt('How many?');
+			produceList.push('Lime ' + qty);
+			showList();
 			break;
 		case 'Barbeque Sauce':
 			var qty = prompt('How many?');
 			condimentsList.push('Barbeque Sauce ' + qty);
+			showList();
 			break;
 		case 'Ketchup':
 			var qty = prompt('How many?');
 			condimentsList.push('Ketchup ' + qty);
+			showList();
 			break;
 		case 'Ranch':
 			var qty = prompt('How many?');
 			condimentsList.push('Ranch ' + qty);
+			showList();
 			break;
 		case 'Mayo':
 			var qty = prompt('How many?');
 			condimentsList.push('Mayo ' + qty);
+			showList();
 			break;
-		case 'Hummus':
+		case 'Olive Oil':
 			var qty = prompt('How many?');
-			snacksList.push('Hummus ' + qty);
+			condimentsList.push('Olive Oil ' + qty);
+			showList();
+			break;
+		case 'Spray Oil':
+			var qty = prompt('How many?');
+			condimentsList.push('Spray Oil ' + qty);
+			showList();
+			break;
+		case 'Buffalo Sauce':
+			var qty = prompt('How many?');
+			condimentsList.push('Buffalo Sauce ' + qty);
+			showList();
+			break;
+		case 'Tabasco':
+			var qty = prompt('How many?');
+			condimentsList.push('Tabasco ' + qty);
+			showList();
+			break;
+		case 'Hot Sauce':
+			var qty = prompt('How many?');
+			condimentsList.push('Hot Sauce ' + qty);
+			showList();
 			break;
 		case 'Cottage Cheese':
 			var qty = prompt('How many?');
 			coldList.push('Cottage Cheese ' + qty);
+			showList();
 			break;
 		case 'Yoghurt':
 			var qty = prompt('How many? (packs of 4)');
 			coldList.push('Yoghurt ' + qty + 'pack(s)');
+			showList();
+			break;
+		case 'Peanut Butter':
+			var qty = prompt('How many?');
+			coldList.push('Peanut Butter ' + qty);
+			showList();
 			break;
 		case 'Lara Bars':
 			var qty = prompt('How many?');
 			snacksList.push('Lara Bars ' + qty);
+			showList();
 			break;
 		case 'Corn Chips':
 			var qty = prompt('How many?');
 			snacksList.push('Corn Chips ' + qty);
+			showList();
+			break;
+		case 'Pop Tarts':
+			var qty = prompt('How many?');
+			snacksList.push('Pop Tarts ' + qty);
+			showList();
+			break;
+		case 'Candy':
+			var qty = prompt('How many (just specify an arbitrary amount)?');
+			snacksList.push('Candy ' + qty);
+			showList();
+			break;
+		case 'Salsa':
+			var qty = prompt('How many?');
+			snacksList.push('Salsa ' + qty);
+			showList();
 			break;
 		case 'Frozen Pizza':
 			var qty = prompt('How many?');
 			coldList.push('Frozen Pizza ' + qty);
+			showList();
 			break;
 		case 'Bag of Fries':
 			var qty = prompt('How many?');
 			coldList.push('Bag of Fries ' + qty + 'bag(s)');
+			showList();
 			break;
 		case 'Ice Cream':
 			var qty = prompt('How many?');
 			coldList.push('Ice Cream ' + qty);
+			showList();
 			break;
 		case 'Shredded Cheese':
 			var qty = prompt('How many?');
 			coldList.push('Shredded Cheese ' + qty);
+			showList();
 			break;
 		case 'Cream Cheese':
 			var qty = prompt('How many?');
 			coldList.push('Cream Cheese ' + qty);
+			showList();
 			break;
 		case 'Sour Cream':
 			var qty = prompt('How many?');
 			coldList.push('Sour Cream ' + qty);
+			showList();
 			break;
-		case 'Chicken':
+		case 'Chicken Breast':
 			var qty = prompt('How many pounds?');
-			meatList.push('Chicken ' + qty + 'lbs');
+			meatList.push('Chicken Breast ' + qty + 'lbs');
+			showList();
 			break;
 		case 'Beef':
 			var qty = prompt('How many pounds?');
 			meatList.push('Beef ' + qty + 'lbs');
+			showList();
 			break;
 		case 'Bacon':
 			var qty = prompt('How many packs?');
 			meatList.push('Bacon ' + qty + 'pack(s)');
+			showList();
 			break;
 		case 'Eggs':
 			var qty = prompt('How many dozens?');
 			meatList.push('Eggs ' + qty + 'pack(s)');
+			showList();
+			break;
+		case 'Chicken Thighs':
+			var qty = prompt('How many pounds?');
+			meatList.push('Chicken Thighs ' + qty + 'lbs');
+			showList();
+			break;
+		case 'Drumsticks':
+			var qty = prompt('How many packs?');
+			meatList.push('Drumsticks ' + qty + 'pack(s)');
+			showList();
 			break;
 		case 'Almond Milk':
 			var qty = prompt('How many?');
 			variousList.push('Almond Milk ' + qty);
+			showList();
 			break;
 		case 'Adam Coffee':
 			var qty = prompt('How many?');
 			variousList.push('Adam Coffee ' + qty);
+			showList();
 			break;
 		case 'Amanda Coffee':
 			var qty = prompt('How many?');
 			variousList.push('Amanda Coffee ' + qty);
+			showList();
 			break;
 		case 'Tea':
 			var qty = prompt('How many boxes?');
 			variousList.push('Tea ' + qty + 'box');
+			showList();
 			break;
 		case 'Honey':
 			var qty = prompt('How many?');
 			variousList.push('Honey ' + qty);
+			showList();
 			break;
 		case 'Cereal':
 			var qty = prompt('How many?');
 			variousList.push('Cereal ' + qty);
+			showList();
 			break;
 		case 'Soup':
 			var qty = prompt('How many cans?');
 			variousList.push('Soup ' + qty + 'cans');
+			showList();
+			break;
+		case 'Broth':
+			var qty = prompt('How many?');
+			variousList.push('Broth ' + qty);
+			showList();
+			break;
+		case 'Tortillas':
+			var qty = prompt('How many bags?');
+			variousList.push('Tortillas ' + qty + 'bags');
+			showList();
 			break;
 	}
 
@@ -195,7 +322,8 @@ function showList() {
 		else {
 		document.getElementById('testButton8').innerHTML += '<li class="listStuff" id="shoppingItem' 
 		+ j + '">' + ' - ' + shoppingList[j] + ' ' + '<button onclick="removeShopping(' + j + ')"' + 
-		'class="removeButtons">' + 'Remove' + '</button>' + '</li>';
+		'class="removeButtons">' + 'Remove' + '</button>' + '<button onclick="moveShopping(' + j + ')"' + 
+		'class="moveButtons">' + 'Move' + '</button>' + '</li>';
 		//The '+=' in the above line is crucial for this to work properly and print the list out
 		}
 	}
@@ -309,6 +437,36 @@ function removeVarious(num) {
 	variousList[num] = '';
 }
 
+//Move item from the Extra/Non-Food list to a different list
+function moveShopping(num) {
+	switch(prompt('Select the list to move the item to (1-6 are your choices).\n' + 
+		'1. Produce 2. Meat 3. Snacks 4. Condiments 5. Various 6. Cold')) {
+		case '1':
+			produceList.push(shoppingList[num]);
+			break;
+		case '2':
+			meatList.push(shoppingList[num]);
+			break;
+		case '3':
+			snacksList.push(shoppingList[num]);
+			break;
+		case '4':
+			condimentsList.push(shoppingList[num]);
+			break;
+		case '5':
+			variousList.push(shoppingList[num]);
+			break;
+		case '6':
+			coldList.push(shoppingList[num]);
+			break;
+		default:
+			console.log('something broke');
+	}
+
+	removeShopping(num);
+	showList();
+}
+
 
 //Opens print window to print the shopping list
 function printList() {
@@ -316,12 +474,3 @@ function printList() {
 	window.print();
 	//Also have this window remove the 'Remove' button first before printing out
 }
-
-
-
-/*
-Other than trying to remove the buttons in the printList() function,
-this project is pretty much done. I just need to make it look pretty and
-make it reponsive. Also, I need to add new items to the frequent list.
-Also, alphebatize the sub category listing.
-*/
